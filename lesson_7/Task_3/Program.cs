cffff0a95094e0d209968c9ec7d29d03875423e9
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-03 23:36:27 +0300

    Draft Task_3

--- lesson_7/Task_3/Program.cs
+++ lesson_7/Task_3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте двумерный массив из трехзначных целых чисел (не менее 100 элементов). 
+В каждом столбце найдите среднее арифметическое среди тех элементов, которые являются палиндромами 
+(если палиндромов нет, то среднее арифметическое считать равным 0). 
+Полученные средние арифметические занести в одномерный массив.
+
+*/
+void FillMatrix(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(100, 1000);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rowCount = 15, columnCount = 10;
+int[,] array = new int[rowCount, columnCount];
+FillMatrix(array);
+// PrintMatrix(array)
+/*Как пробежаться по столбцам?
+Подумать над применением цикла foreach для определения палиндрома
+Найти сумму палиндромов и их количество
+Найти среднее арифметическое
+каждый столбец - это элемент одномерного массива
+*/
