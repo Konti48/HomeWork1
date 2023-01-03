2c3c566ccac6e8782249a61b5211742d353a3ec7
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-03 21:01:18 +0300

    Complete Task_1

--- lesson_7/Task_1/Program.cs
+++ lesson_7/Task_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] table = new double[m, n];
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        double ranDouble = Math.Round(rnd.NextDouble(), 2);
+        int ranInteger = rnd.Next(0, 10);
+        table[i, j] = ranInteger + ranDouble;
+        Console.Write(table[i, j] + " ");
+    }
+    Console.WriteLine();
+}
