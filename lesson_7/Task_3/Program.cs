3e40f40b5421996b475e255356b067b0f8fea7d1
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-05 18:48:21 +0300

    Completet Task_3

--- lesson_7/Task_3/Program.cs
+++ lesson_7/Task_3/Program.cs
@@ -3,6 +3,11 @@
 (если палиндромов нет, то среднее арифметическое считать равным 0). 
 Полученные средние арифметические занести в одномерный массив.
 
+Как пробежаться по столбцам?
+Найти палиндром
+Найти сумму палиндромов и их количество
+Найти среднее арифметическое
+каждый столбец - это элемент одномерного массива
 */
 void FillMatrix(int[,] matrix)
 {
@@ -28,13 +33,26 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-int rowCount = 15, columnCount = 10;
+int rowCount = 10, columnCount = 20;
 int[,] array = new int[rowCount, columnCount];
 FillMatrix(array);
-// PrintMatrix(array)
-/*Как пробежаться по столбцам?
-Подумать над применением цикла foreach для определения палиндрома
-Найти сумму палиндромов и их количество
-Найти среднее арифметическое
-каждый столбец - это элемент одномерного массива
-*/
+PrintMatrix(array);
+Console.WriteLine();
+double[] newArray = new double[columnCount];
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int total = 0;
+    int count = 0;
+    double meanValue = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i, j] % 10 == array[i, j] / 100)
+        {
+            total += array[i, j];
+            count++;
+            meanValue = total / count;
+        }
+    }
+    newArray[j] = meanValue;
+    Console.Write(newArray[j] + " ");
+}
