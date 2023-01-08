1b3998f81e8e3904f24b1ecf74a49129343868b0
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-09 01:14:20 +0300

    Add Task_4

---
+++ lesson_8/Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Сформируйте двухмерный массив из неповторяющихся случайных двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив. 
+Обратите внимание, что максимальный размер такого массива ограничен. 
+Проверку эл-та на присутствие в массиве можно вынести в отдельную функцию.
+
+Количество возможных элементов - от 10 до 99 = 90, т.е  m * n <= 90
+Пользователь задаёт m и n,  происходит проверка
+
+в методе создания массива необходима проверка на повторяемость
+
+
+*/
+void FillMatrix(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void ChekRepeat(int q)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) ;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
