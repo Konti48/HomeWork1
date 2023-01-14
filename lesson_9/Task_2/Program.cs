b629d561c58ea2c3007c9a53f4c18c22ed1fd46f
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-14 23:27:44 +0300

    complet task 2

--- lesson_9/Task_2/Program.cs
+++ lesson_9/Task_2/Program.cs
@@ -1,11 +1,12 @@
 ﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
  Выполнить с помощью рекурсии.
 */
-int m = 8, n = 18;
+int m = 3;
+int n = 5;
 int GetSumUntilNums(int a, int b)
 {
     if (b == a) return a;
-    else return b + GetSumUntilNums(b - 1);
+    else return b + GetSumUntilNums(a, b - 1);
 }
 
 Console.WriteLine(GetSumUntilNums(m, n));
