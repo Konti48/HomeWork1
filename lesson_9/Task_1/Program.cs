2a517d51d4f26bc25f12cd620febc5b7a49d72bf
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-14 23:00:37 +0300

    complet task 1

--- lesson_9/Task_1/Program.cs
+++ lesson_9/Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+*/
+int num = 9;
+int PrintRow(int n)
+{
+    Console.Write(n + " "); 
+
+    if (n == 1) return 1;
+    else return PrintRow(n - 1);
+}
+PrintRow(num);
